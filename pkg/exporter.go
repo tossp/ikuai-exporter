484b694fa8a166f5263051083bbd693999525a15
砼砼 <code@tossp.com>
2025-01-26 07:02:59 +0800

    feat(core): 添加IPv6支持

--- pkg/exporter.go
+++ pkg/exporter.go
@@ -150,13 +150,20 @@ func (i *IKuaiExporter) Collect(metrics chan<- prometheus.Metric) {
 	if isFail(&lanDevice.Result, err) {
 		log.Printf("ikuai ShowMonitorLan: %v, %+v", err, lanDevice.Result)
 	} else {
-		devices := map[string]action.LanDeviceInfo{}
+		devices := make(map[string]*action.LanDeviceInfo)
 
 		for _, device := range lanDevice.Data.Data {
-			deviceId := fmt.Sprintf("device/%v", device.IPAddr)
+			deviceId := fmt.Sprintf("device/%v", device.Mac)
 
-			if _, ok := devices[deviceId]; !ok {
-				devices[deviceId] = device
+			if _, has := devices[deviceId]; has {
+				devices[deviceId].TotalUp += device.TotalUp
+				devices[deviceId].TotalDown += device.TotalDown
+				devices[deviceId].Upload += device.Upload
+				devices[deviceId].Download += device.Download
+				devices[deviceId].ConnectNum += device.ConnectNum
+				devices[deviceId].IPAddr += "|" + device.IPAddr
+			} else {
+				devices[deviceId] = &action.LanDeviceInfo{Mac: device.Mac, Hostname: device.Hostname, IPAddr: device.IPAddr, Comment: device.Comment}
 			}
 		}
 
